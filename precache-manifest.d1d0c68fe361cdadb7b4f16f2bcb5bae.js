cce1c5c7d39780565a4671cb5c8f7ec861159093
Alex Wasser <awasser@omnispear.com>
2019-08-23 14:58:58 -0400

    Updates

---
+++ precache-manifest.d1d0c68fe361cdadb7b4f16f2bcb5bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c324b8bb8fa5db52f9a1b600e1877fe",
+    "url": "/gh-tracker/index.html"
+  },
+  {
+    "revision": "755066f2b496bafff77f",
+    "url": "/gh-tracker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9c5557ea7b78905731df",
+    "url": "/gh-tracker/static/js/2.c0b87aa3.chunk.js"
+  },
+  {
+    "revision": "755066f2b496bafff77f",
+    "url": "/gh-tracker/static/js/main.fa178e8b.chunk.js"
+  },
+  {
+    "revision": "b2ec7b48d3b0a98f51f6",
+    "url": "/gh-tracker/static/js/runtime~main.004d3426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
